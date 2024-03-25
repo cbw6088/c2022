a8c520e77dadd3ed3456c40fe07c8074949ed720
ChoiByungwoo <cbw4186@naver.com>
2024-03-25 18:00:36 +0900

    update

--- JavaScript/Backjoon/9012.js
+++ JavaScript/Backjoon/9012.js
@@ -1,25 +1,24 @@
 const fs = require('fs');
-const filename = "JavaScript/Backjoon/input.txt"
-const valueData = fs.readFileSync(filename, "utf8").toString().trim().split("\n");
-// const valueData = fs.readFileSync('/dev/stdin','utf-8').toString().trim().split('\n')
+// const filename = "JavaScript/Backjoon/input.txt"
+// const valueData = fs.readFileSync(filename, "utf8").toString().trim().split("\n");
+const valueData = fs.readFileSync('/dev/stdin','utf-8').toString().trim().split('\n')
 
 const cnt = parseInt(valueData[0]);
-const stack = []
 
 for (let i = 1; i <= cnt; i++) {
+    var result = 'YES'
+    const stack = []
     for(let j = 0; j < valueData[i].length; j++){
         if(valueData[i][j] == '('){
             stack.push(valueData[i][j]);
         }
         else if(valueData[i][j] == ')'){
-            if(stack[stack.length - 1] != '('){
-                console.log('NO');
-                console.log(i);
+            if(!stack.pop()){
+                result = 'NO';
                 break;
             }
-            else stack.pop();
         }
     }
-    if(stack[stack.length-1] == 0) console.log('YES');
-    console.log(stack);
+    if(stack.length != 0) result = 'NO';
+    console.log(result);
 }
